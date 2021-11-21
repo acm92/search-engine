c81d7595a2ff1275df0d5bfdae04802ba9ee8d3d
angelcm92 <angelcld7@gmail.com>
2021-11-21 18:57:04 +0100

    First commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const HeaderComponent = () => (
+
+    <header className="Header">
+        <h1 className="Header__title">Image search</h1>
+        <h4 className="Header__subtitle">Look for what you have in mind and we'll display it nicely</h4>
+    </header>
+
+)
+
+export default HeaderComponent
