ae2221e45f1f3f3e11f46ac2e9fb9a1718054259
AngelC <angelcld7@gmail.com>
2024-03-18 14:59:34 +0100

    Responsive design

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,12 +1,28 @@
-import React from "react"
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
-const HeaderComponent = () => (
+const HeaderComponent = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
+  if (isTabletOrMobile) {
+    return (
+      <header className="Header-Phone">
+        <h1 className="Header-Phone__title">Image search</h1>
+        <h4 className="Header-Phone__subtitle">
+          Look for what you have in mind and we'll display it nicely
+        </h4>
+      </header>
+    );
+  }
+
+  return (
     <header className="Header">
-        <h1 className="Header__title">Image search</h1>
-        <h4 className="Header__subtitle">Look for what you have in mind and we'll display it nicely</h4>
+      <h1 className="Header__title">Image search</h1>
+      <h4 className="Header__subtitle">
+        Look for what you have in mind and we'll display it nicely
+      </h4>
     </header>
+  );
+};
 
-)
-
-export default HeaderComponent
+export default HeaderComponent;
--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,13 +1,34 @@
-import React from "react"
+import React from "react";
+import { useMediaQuery } from "react-responsive";
 
+const ImageComponent = (props) => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
 
-const ImageComponent = (props) => (
+  if (isTabletOrMobile) {
+    return (
+      <div className="image-container">
+        <a href={props.imag.urls.full}>
+          <img
+            className="image-phone"
+            alt={props.imag.alt_description}
+            src={props.imag.urls.small}
+          />
+        </a>
+      </div>
+    );
+  }
 
+  return (
     <div className="image-container">
-        <a href={props.imag.urls.full}><img className="image" alt={props.imag.alt_description} src={props.imag.urls.small} /></a>
+      <a href={props.imag.urls.full}>
+        <img
+          className="image"
+          alt={props.imag.alt_description}
+          src={props.imag.urls.small}
+        />
+      </a>
     </div>
-)
-      
-export default ImageComponent
-    
+  );
+};
 
+export default ImageComponent;
