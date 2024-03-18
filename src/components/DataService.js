3bb74eeb4190d3fe19e4f4b8c44cba28774ced02
AngelC <angelcld7@gmail.com>
2024-03-18 14:59:01 +0100

    final changes

--- src/components/DataService.js
+++ src/components/DataService.js
@@ -1,20 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
 class DataService {
+  retrieveImages(searchResult) {
+    const instance = axios.create({
+      baseURL: "https://api.unsplash.com",
+      timeout: 1000,
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
 
-
-    retrieveImages(searchResult) {
-
-        const instance = axios.create({
-            baseURL: 'https://api.unsplash.com',
-            timeout: 1000,
-            headers: { 'Authorization': 'Client-ID MjBDc39EslwFPEK7RTf0tHAav81KM3O3MMTeD_kmY5Y' }
-        })
-
-        return instance.get(`https://api.unsplash.com/search/photos?query=${searchResult}`)
-    }
-
-
+    return instance.get(
+      `https://api.unsplash.com/search/photos?query=${searchResult}`
+    );
+  }
 }
 
-export default new DataService()
+export default new DataService();
