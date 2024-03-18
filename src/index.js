c7eb3a0d069cc0394bc6df3b8c8ad479c1cd5a30
AngelC <angelcld7@gmail.com>
2024-03-18 16:00:17 +0100

    added node_modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter Basename={process.env.PUBLIC_URL}>
